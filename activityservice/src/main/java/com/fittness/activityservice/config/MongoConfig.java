a4fdb6a01bd4efb5c70f9685d6a1382d17b27117
ArjunKhade <khadearjun@gmail.com>
2026-01-12 21:05:24 +0530

    created project for activity service

---
+++ activityservice/src/main/java/com/fittness/activityservice/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package com.fittness.activityservice.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+
+}
